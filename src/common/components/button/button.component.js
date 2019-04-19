342c28ad8b5e921493d01f7fa1f140b7e066514e
AndriiSendun <andrii.sendun@gmail.com>
2019-04-19 08:03:20 +0300

    create button component

---
+++ src/common/components/button/button.component.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./button.styles.scss";
+
+export const TYPES = {
+  PRIMARY: "btn--primary",
+  SECONDARY: "btn--secondary"
+};
+
+export const Button = ({ title, buttonType, onClick, disabled, type }) => (
+  <button
+    type={type}
+    className={`${buttonType} btn`}
+    disabled={disabled}
+    onClick={onClick}
+  >
+    {title}
+  </button>
+);
+
+export default Button;
