78b5192da26f01ecfb3ec7e36eaa36f08882c3e2
AndriiSendun <andrii.sendun@gmail.com>
2019-04-22 11:10:26 +0300

    create home-page component

---
+++ src/pages/home/home.component.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { push } from "connected-react-router";
+import { connect } from "react-redux";
+
+import { Button, TYPES } from "../../common/components/button/button.component";
+
+import homeImg from "../../assets/home-page.png";
+import "./home.styles.scss";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="home">
+        <h2 className="home__title">
+          <span className="title-span">Your home.</span>
+          <span className="title-span title-span--green">Planty</span>
+        </h2>
+        <h3 className="home__subtitle">Enjoy the experiance</h3>
+        <div className="home__img">
+          <img className="img" src={homeImg} alt="home" />
+        </div>
+
+        <div className="buttons">
+          <Button
+            title="Log In"
+            buttonType={TYPES.PRIMARY}
+            onClick={() => {
+              this.props.push("log-in");
+            }}
+          />
+          <Button
+            title="Sign Up"
+            buttonType={TYPES.SECONDARY}
+            onClick={() => {
+              this.props.push("sign-up");
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  { Home, push }
+)(Home);
