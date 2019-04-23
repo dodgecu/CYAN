93a0c47e74bebdddbc50fe267b8a4e46a37d50fc
dodgecu <rogertaylor1949@gmail.com>
2019-04-23 12:44:38 +0300

    Add flower types constant along with their images

---
+++ src/constants/flowers.js
@@ -0,0 +1,16 @@
+const flowers = [
+  { id: 1, name: "Rose of China", flower_img: "../../assets/home-page.png" },
+  {
+    id: 2,
+    name: "Flowering Maple",
+    flower_img: "../../assets/plant-image.jpg"
+  },
+  { id: 3, name: "Flamingo Flower" },
+  { id: 4, name: "Kaffir Lily" },
+  { id: 5, name: "Flaming Katy" },
+  { id: 6, name: "Lollipop Plant" },
+  { id: 7, name: "Moth Orchid" },
+  { id: 8, name: "Zebra Plant" }
+];
+
+export default flowers;
---
+++ src/pages/flower-details/flower-details.component.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class FlowerDetails extends Component {
+  render() {
+    return <h1>Flower Details</h1>;
+  }
+}
+
+export default FlowerDetails;
