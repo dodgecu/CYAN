8c31afd41f88fca825aed679a2e14e80d1e0923e
m-ost <ostashmariya@gmail.com>
2019-04-28 20:28:43 +0300

    update user-profile

---
+++ src/pages/authorization/user-profile/user-profile.delete.component.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { reduxForm } from "redux-form";
+import { push } from "connected-react-router";
+
+import { deleteUser } from "../authorization.action";
+import "./user-profile.scss";
+
+import {
+  Button,
+  TYPES
+} from "../../../common/components/button/button.component";
+
+class DeleteProfile extends Component {
+  render() {
+    return (
+      <div className="authorization--form">
+        <h2>Delete account</h2>
+        <p>
+          This account will be permanently deleted. After this, you will not be
+          able to restore it again.
+        </p>
+        <Button
+          title="DELETE"
+          onClick={() => this.props.deleteUser()}
+          buttonType={TYPES.DELETE}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  message: state.authReducer.message
+});
+
+DeleteProfile = reduxForm({
+  form: "delete"
+})(DeleteProfile);
+
+export default connect(
+  mapStateToProps,
+  { deleteUser, push }
+)(DeleteProfile);
---
+++ src/pages/authorization/user-profile/user-profile.name.component.js
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Field, reduxForm } from "redux-form";
+
+import { updateUser } from "../authorization.action";
+import "./user-profile.scss";
+
+import {
+  Button,
+  TYPES
+} from "../../../common/components/button/button.component";
+import Input from "../../../common/components/input/input.component";
+
+import { maxLength15 } from "../../../common/form-validation";
+
+class UpdateName extends Component {
+  onSubmit(inputValue) {
+    this.props.updateUser(inputValue);
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <>
+        <div className="authorization--form">
+          <h4>Personal info</h4>
+          <form
+            onSubmit={handleSubmit(value =>
+              this.onSubmit.bind(this)({ name: value.name })
+            )}
+          >
+            <Field
+              name="name"
+              type="text"
+              component={Input}
+              label="Username"
+              validate={maxLength15}
+            />
+            <Button title="UPDATE" type="submit" buttonType={TYPES.PRIMARY} />
+          </form>
+        </div>
+      </>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  message: state.authReducer.message
+});
+
+UpdateName = reduxForm({
+  form: "update-name"
+})(UpdateName);
+
+export default connect(
+  mapStateToProps,
+  { updateUser }
+)(UpdateName);
