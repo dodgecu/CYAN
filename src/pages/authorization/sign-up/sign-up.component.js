4f8f5826d9968353104e5dbaeb85db464e3d6451
Pavlo Pihula <rogertaylor1949@gmail.com>
2019-04-30 09:44:10 -0400

    Bug fix

--- src/pages/authorization/sign-up/sign-up.component.js
+++ src/pages/authorization/sign-up/sign-up.component.js
@@ -4,7 +4,6 @@ import { Field, reduxForm } from "redux-form";
 import { push } from "connected-react-router";
 
 import { register } from "../authorization.action";
-import { falseRegistered } from "../authorization.action";
 import {
   required,
   email,
@@ -20,7 +19,6 @@ import Input from "../../../common/components/input/input.component";
 import Header from "../../../common/header/header.component";
 import CustomLink from "../../../common/components/custom-link/custom-link.component";
 import PageTitle from "./../../../common/page-title/page-title.component";
-import Spinner from "./../../../common/components/spinner/spinner.component";
 
 import routes from "../../../constants/routes";
 
@@ -42,7 +40,6 @@ class SignUp extends Component {
 
     return (
       <>
-        {this.props.isLoading ? <Spinner /> : null}
         <Header />
         <PageTitle title="Register to Cyander" />
         <div className="authorization authorization--sign-up">
@@ -79,8 +76,8 @@ class SignUp extends Component {
               placeholder="Repeat password"
               validate={[required, passwordsMatch]}
             />
-            {this.props.message ? (
-              <div className="error-message">{this.props.message}</div>
+            {this.props.message === "User already exists" ? (
+              <div>{this.props.message}</div>
             ) : null}
             <Button
               title="SIGN UP"
@@ -105,8 +102,7 @@ class SignUp extends Component {
 
 const mapStateToProps = state => ({
   isRegistered: state.authReducer.isRegistered,
-  message: state.authReducer.message,
-  isLoading: state.authReducer.isLoading
+  message: state.authReducer.message
 });
 
 SignUp = reduxForm({
@@ -115,5 +111,5 @@ SignUp = reduxForm({
 
 export default connect(
   mapStateToProps,
-  { register, push, falseRegistered }
+  { register, push }
 )(SignUp);
