d5e92282649c5143171d9efec17ef4ed068575d0
m-ost <ostashmariya@gmail.com>
2019-05-04 21:06:12 +0300

    edit dashboard

--- src/pages/flower-thumbnail/flower-thumbnail.js
+++ src/pages/flower-thumbnail/flower-thumbnail.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { connect } from "react-redux";
-import { push } from "connected-react-router";
-
-import routes from "./../../constants/routes";
 import "./flower-thumbnail.scss";
 import PlantImage from "../../assets/plant-image.png";
 
 function FlowerThumbnail(props) {
-  const redirect = () => props.push(routes.flowerDetails, { flower: props.id });
   return (
     <div className="thumbnail">
       <div className="thumbnail--flower">
@@ -25,7 +20,7 @@ function FlowerThumbnail(props) {
               className="range--temperature__fill"
               style={{ width: `${props.airTemperature}%` }}
             />
-            <span className="percentage">{props.airTemperature}</span>
+            <span className="percentage">`{props.airTemperature}%`</span>
           </div>
           <span className="ranges__title">Humidity</span>
           <div className="range range--humidity">
@@ -33,8 +28,7 @@ function FlowerThumbnail(props) {
               className="range--humidity__fill"
               style={{ width: `${props.airHumidity}%` }}
             />
-
-            <span className="percentage">{props.airHumidity}</span>
+            <span className="percentage">`{props.airHumidity}%`</span>
           </div>
           <span className="ranges__title">Ambient light</span>
           <div className="range range--light">
@@ -42,7 +36,7 @@ function FlowerThumbnail(props) {
               className="range--light__fill"
               style={{ width: `${props.ambientLight}%` }}
             />
-            <span className="percentage">{props.ambientLight}</span>
+            <span className="percentage">`{props.ambientLight}%`</span>
           </div>
           <span className="ranges__title">Soil humidity</span>
           <div className="range range--soil">
@@ -50,18 +44,15 @@ function FlowerThumbnail(props) {
               className="range--soil__fill"
               style={{ width: `${props.soilHumidity}%` }}
             />
-            <span className="percentage">{props.soilHumidity}</span>
+            <span className="percentage">`{props.soilHumidity}%`</span>
           </div>
         </div>
       </div>
-      <span className="details" onClick={() => redirect()}>
+      <a className="details" href={`/flower-details/${props.id}`}>
         View details
-      </span>
+      </a>
     </div>
   );
 }
 
-export default connect(
-  null,
-  { push }
-)(FlowerThumbnail);
+export default FlowerThumbnail;
