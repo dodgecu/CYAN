bdf0cf61e3c9b3f0d5267ab5bd891f9afc5b428f
Andrii Sendun <andrii.sendun@thomsonreuters.com>
2019-05-15 17:31:55 +0300

    change no data chart style component

--- src/pages/flower-details/chart/no-data-chart.component.js
+++ src/pages/flower-details/chart/no-data-chart.component.js
@@ -6,7 +6,8 @@ function NoDataChart(props) {
   return (
     <div className={`error-chart error-chart--${props.selector}`}>
       <h2 className="error-chart__title">
-        WARNING! There is no statistic about flower on this day
+        <i className="material-icons material-icons--warning">warning</i>
+        <span>There is no statistic about flower on this day</span>
       </h2>
     </div>
   );
